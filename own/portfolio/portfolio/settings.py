f6346d8738e4ba9bd61d3c0789c8f9254a4e4077
RITIK KUMAR <73395641+RitikKumar121@users.noreply.github.com>
2021-06-04 20:16:04 +0530

    commit

--- own/portfolio/portfolio/settings.py
+++ own/portfolio/portfolio/settings.py
@@ -53,7 +53,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-     
+     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -135,3 +135,6 @@ STATIC_URL = '/static/'
 #    STATIC_DIR,
 #]
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "live-static-files", "media-root")
