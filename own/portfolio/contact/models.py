b07a3f6a98fab392f74249628260648e7569658f
RITIK KUMAR <73395641+RitikKumar121@users.noreply.github.com>
2021-06-06 14:59:55 +0530

    commit

--- own/portfolio/contact/models.py
+++ own/portfolio/contact/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 class Connector(models.Model):
-    naam=models.CharField(null=True,max_length=200)
-    last=models.CharField(null=True,max_length=200)
-    gmail=models.EmailField(null=True)
-    comment=models.TextField(null=True)
+    naam=models.CharField(max_length=200)
+    last=models.CharField(max_length=200)
+    gmail=models.EmailField()
+    comment=models.TextField()
 
     def __str__(self):
         return f"{self.naam},{self.last}"
