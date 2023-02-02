7bda9fc0ee5ff3efba80bbcc7a8ba10e681badeb
RITIK KUMAR <73395641+RitikKumar121@users.noreply.github.com>
2023-02-02 13:03:06 +0530

    Update settings.py

--- own/portfolio/portfolio/settings.py
+++ own/portfolio/portfolio/settings.py
@@ -143,9 +143,9 @@ STATICFILES_DIRS = (
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-MESSAGE_TAGS = {
-    messages.ERROR:'danger'
-}
+#DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+#MESSAGE_TAGS = {
+ #   messages.ERROR:'danger'
+#}
 MEDIA_URL="/media/"
 MEDIA_ROOT=os.path.join(BASE_DIR,"media/")
