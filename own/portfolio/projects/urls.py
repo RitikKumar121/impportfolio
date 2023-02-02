ffaf34b038acfff4fb2926dd55bc1538acc5a4e5
RITIK KUMAR <73395641+RitikKumar121@users.noreply.github.com>
2023-02-02 13:08:10 +0530

    Update urls.py

--- own/portfolio/projects/urls.py
+++ own/portfolio/projects/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from projects import views
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
  path('projects.html/',views.projects),
 
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG:
+    urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
